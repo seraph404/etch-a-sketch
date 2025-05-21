000123f2d1f86c63f5280d4e430a971293116283
Seraphina <211722638+seraph404@users.noreply.github.com>
2025-05-21 12:07:07 -0400

    Create drawGrid() function

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('hi');
+// set container size
+let containerSize = 600;
+const container = document.querySelector('#container');
+container.style.width = containerSize + "px";
+container.style.height = containerSize + "px";
+
+// draw the grid on the page
+function drawGrid(gridSize) {
+    const divSize = (containerSize / gridSize) - 2;
+    gridSize = gridSize ** 2;
+
+    for (let i = 0; i < gridSize; i++) {
+        let div = document.createElement('div');
+        
+        div.style.width = divSize + "px";
+        div.style.height = divSize + "px";
+        container.append(div);
+    }
+}
+
+drawGrid(16);
