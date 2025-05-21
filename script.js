0c81ecf474d662f7801d27c482aed2bb276f5917
Seraphina <211722638+seraph404@users.noreply.github.com>
2025-05-21 12:36:32 -0400

    Move container variables inside of drawGrid()

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-// set container size
-let containerSize = 600;
-const container = document.querySelector('#container');
-container.style.width = containerSize + "px";
-container.style.height = containerSize + "px";
+
 
 // draw the grid on the page
 function drawGrid(gridSize) {
+    // set container size
+    let containerSize = 600;
+    const container = document.querySelector('#container');
+    container.style.width = containerSize + "px";
+    container.style.height = containerSize + "px";
     const divSize = (containerSize / gridSize) - 2;
     gridSize = gridSize ** 2;
 
